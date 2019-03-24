664bd316c1b2b2ec44de13906f165e88aad5ae2d
David Cruz Anaya <me@davidcruz.co.uk>
2019-03-24 11:40:00 +0000

    Add clarification in to LifecycleTest class

---
+++ app/src/main/java/uk/co/davidcruz/myapplication/Methods.kt
@@ -0,0 +1,5 @@
+package uk.co.davidcruz.myapplication
+
+fun Int.add(number: Int): Int {
+    return this + number
+}
--- app/src/test/java/uk/co/davidcruz/myapplication/LifecycleTest.kt
+++ app/src/test/java/uk/co/davidcruz/myapplication/LifecycleTest.kt
@@ -9,10 +9,11 @@ import org.junit.jupiter.api.Assertions.assertEquals
  * 2. Adding @TestInstance(TestInstance.Lifecycle.PER_CLASS) above the class (Applied)
  */
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@TestInstance(TestInstance.Lifecycle.PER_METHOD) /* Way 1 Default */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) /* Way 2 */
 class LifecycleTest {
 
-//    /* Way 1 */
+//    /* Way 1 - Start* /
 //    companion object {
 //
 //        @JvmStatic
@@ -27,20 +28,23 @@ class LifecycleTest {
 //            println("4. this will be execute in he end after latest @AfterEach in case this exist")
 //        }
 //    }
+//    /* Way 1 - End */
 
+    /* Way 2 - Start */
     @BeforeAll
     fun beforeAllTest() {
         println("1. this is execute before @BeforeEach in case this exist")
     }
 
+    @AfterAll
+    fun afterAllTest() {
+        println("4. this will be execute in he end after latest @AfterEach in case this exist")
+    }
+    /* Way 2 - End */
+
     @BeforeEach
     fun beforeEachTest() {
         println("2. this is execute before each Unit Test in this file")
-    }
-
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
     }
 
     @AfterEach
@@ -48,9 +52,9 @@ class LifecycleTest {
         println("3. this is execute after each Unit Test in this file")
     }
 
-    @AfterAll
-    fun afterAllTest() {
-        println("4. this will be execute in he end after latest @AfterEach in case this exist")
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2.add(2))
     }
 
 }
