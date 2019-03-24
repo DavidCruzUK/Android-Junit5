23327d93adc4b8ef1faa8c9c36bbcda2c2403841
David Cruz Anaya <me@davidcruz.co.uk>
2019-03-24 20:12:50 +0000

    Add Extra assertion and a clarification note

--- app/src/test/java/uk/co/davidcruz/myapplication/DisplayNameTest.kt
+++ app/src/test/java/uk/co/davidcruz/myapplication/DisplayNameTest.kt
@@ -1,11 +1,10 @@
 package uk.co.davidcruz.myapplication
 
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
-@DisplayName("Testing a static useless list of names")
+@DisplayName("Testing a static useless list of names using Assert")
 class DisplayNameTest {
 
     private val listOfNames = listOf("David", "Paul", "Jessica", "Abdul")
@@ -22,6 +21,20 @@ class DisplayNameTest {
     fun nameIsInNotTheListTest() {
         val nameToSearch = "Raul"
         assertFalse(nameToSearch in listOfNames)
+    }
+
+    /**
+     * Note:
+     * Using Assertions.assertEquals() is like using in java: name1.equals(name2)
+     * Using Assertions.assertSame() is like using in java: name1 == name2
+     */
+
+    @DisplayName("Given a two variables with same value, when assertSame() will fail")
+    @Test
+    fun testTwoDifferentVariablesWithSameValue() {
+        val nameToCompare = listOfNames[1]
+        val secondNameToCompare = listOfNames[1]
+        assertEquals(nameToCompare, secondNameToCompare, "Is not Equals")
     }
 
 }
